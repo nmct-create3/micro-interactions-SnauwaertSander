3da4733e069c4c5eecd1db93ab5e937e083ce738
SnauwaertSander <55881160+SnauwaertSander@users.noreply.github.com>
2020-10-06 12:22:54 +0200

    commit

--- script/app.js
+++ script/app.js
@@ -1,13 +1,20 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  var b = document.getElementById('togglePassword');
+  var x = document.getElementById('password');
+
+  b.addEventListener('click', function () {
+    if (x.type === 'password') {
+      x.type = 'text';
+    } else {
+      x.type = 'password';
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Script loaded!');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
